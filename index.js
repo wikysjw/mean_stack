f842e40ec3b4cfebf6eb3da442981d7bc3f6036d
wikysjw <43368944+wikysjw@users.noreply.github.com>
2018-11-13 13:40:39 +0900

    back end 개발

--- index.js
+++ index.js
@@ -24,6 +24,7 @@ app.use(methodOverride("_method"));
 
 // Routes
 app.use("/", require("./routes/home"));
+app.use("/posts", require("./routes/posts"));
 
 // Port setting
 app.listen(5034, function(){

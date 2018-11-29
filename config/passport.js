062ec10c07f7077244b37612c7feb3b20b825c18
wikysjw <43368944+wikysjw@users.noreply.github.com>
2018-11-29 14:29:51 +0900

    welcome 페이지 동영상 삽임
    나이키페이지 소스 사용

--- config/passport.js
+++ config/passport.js
@@ -30,7 +30,7 @@ passport.use("local-login",
              return done(null, user);
          } else {
              req.flash("username", username);
-             req.flash("errors", {login:"아이디이나 비밀번호가 다릅니다."});
+             req.flash("errors", {login:"아이디이나 비밀번호가 일치하지 않습니다."});
              return done(null, false); // done의 첫번째 파라미터는 error을 담기위한 용도 error이 없다면 null을 사용
          }
      });

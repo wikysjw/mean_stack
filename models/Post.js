7c1f0ca9b6b3d43b54cbf906161b23ca9b79dcfe
wikysjw <43368944+wikysjw@users.noreply.github.com>
2018-11-30 10:40:54 +0900

    게시판 제목없이 작성 오류 수정
    게시판 제목없이 작성 오류 수정

--- models/Post.js
+++ models/Post.js
@@ -1,9 +1,10 @@
 var mongoose = require("mongoose");
+var util = require("../util");
 
 // schema
 var postSchema = mongoose.Schema({
-    title:{type:String, require:true},
-    body:{type:String},
+    title:{type:String, required:[true,"제목을 적어주세요."]},
+    body:{type:String, required:[true,"본문을 작성해주세요."]},
     createdAt:{type:Date, default:Date.now},
     updated:{type:Date}
 },{
@@ -13,22 +14,22 @@ var postSchema = mongoose.Schema({
 // virtuals
 postSchema.virtual("createdDate")
 .get(function(){
-    return getDate(this.createdAt);
+    return util.getDate(this.createdAt);
 });
 
 postSchema.virtual("createdTime")
 .get(function(){
-    return getTime(this.createdAt);
+    return util.getTime(this.createdAt);
 });
 
 postSchema.virtual("updatedDate")
 .get(function(){
-    return getTime(this.updatedAt);
+    return util.getDate(this.updatedAt);
 });
 
-postSchema.virtual("updatedDate")
+postSchema.virtual("updatedTime")
 .get(function(){
-    return getTime(this.updatedAt);
+    return util.getTime(this.updatedAt);
 });
 
 // model & export
@@ -36,16 +37,16 @@ var Post = mongoose.model("post", postSchema);
 module.exports = Post;
 
 // functions
-function getDate(dateObj){
-    if(dateObj instanceof Date)
-    return dateObj.getFullYear() + "-" + get2digits(dateObj.getMonth()+1)+ "-" + get2digits(dateObj.getDate());
-}
+// function getDate(dateObj){
+//     if(dateObj instanceof Date)
+//     return dateObj.getFullYear() + "-" + get2digits(dateObj.getMonth()+1)+ "-" + get2digits(dateObj.getDate());
+// }
 
-function getTime(dateObj){
-    if(dateObj instanceof Date)
-    return get2digits(dateObj.getHours()) + "-" + get2digits(dateObj.getMinutes())+ "-" +get2digits(dateObj.getSeconds());
-}
+// function getTime(dateObj){
+//     if(dateObj instanceof Date)
+//     return get2digits(dateObj.getHours()) + "-" + get2digits(dateObj.getMinutes())+ "-" +get2digits(dateObj.getSeconds());
+// }
 
-function get2digits(num){
-    return ("0" + num).slice(-2);
-}
+// function get2digits(num){
+//     return ("0" + num).slice(-2);
+// }
